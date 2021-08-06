dfd768f129252f081029f309f94b1c3a32d2cdf8
Vjet Node JS <51021128+huuhoa2309@users.noreply.github.com>
2021-08-07 02:12:26 +0700

    connectDB with Sequelize & render data

--- src/server.js
+++ src/server.js
@@ -2,11 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRouters from "./route/web";
+import connectDB from './config/connectDB';
 require('dotenv').config();
 
 let app= express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+viewEngine(app);
+initWebRouters(app);
+connectDB();
+
 let port= process.env.PORT || 8081; //post === undefine => port 8081
 app.listen( port, ()=>{
     console.log("Backend running in port:"+port);
@@ -15,6 +23,5 @@ app.listen( port, ()=>{
 )
 
 
-viewEngine(app);
-initWebRouters(app);
+
 
