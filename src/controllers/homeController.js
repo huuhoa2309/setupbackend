f75a4e99480b5d35557836c64a6e86b555303eac
Vjet Node JS <51021128+huuhoa2309@users.noreply.github.com>
2021-08-11 01:30:31 +0700

    config ORM service

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,24 +1,33 @@
-import db from '../models/index';
+import db from "../models/index";
+import CRUDService from "../services/CRUDService";
 
-
-let getHomePage = async(req, res) => {
-    try{
-        let data= await db.User.findAll();
-        return res.render('homepage.ejs', {
-            data: JSON.stringify(data)
-        });
-    }catch(e) {
-        console.log(e)
-    }
-
-    
-}
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+    return res.render("homepage.ejs", {
+      data: JSON.stringify(data),
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 let getTestapi = (req, res) => {
-    return res.render('test/testapi.ejs');
-}
-
+  return res.render("test/testapi.ejs");
+};
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  console.log("-----------infomation data-----------------");
+  console.log(req.body);
+  res.send("psot crud sucsessfull :))");
+};
 module.exports = {
-    getHomePage: getHomePage,
-    getTestapi: getTestapi
-}
+  getHomePage: getHomePage,
+  getTestapi: getTestapi,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
+};
--- src/route/web.js
+++ src/route/web.js
@@ -4,10 +4,12 @@ import homeController from "../controllers/homeController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/', homeController.getHomePage);
-    router.get('/api', homeController.getTestapi);
+  router.get("/", homeController.getHomePage);
+  router.get("/api", homeController.getTestapi);
+  router.get("/crud", homeController.getCRUD);
+  router.post("/post-crud", homeController.postCRUD);
 
-    return app.use("/", router);
-}
+  return app.use("/", router);
+};
 
 module.exports = initWebRoutes;
