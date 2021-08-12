7888d0325028346cfdaceb01d7b88c72c4c1dd50
Vjet Node JS <51021128+huuhoa2309@users.noreply.github.com>
2021-08-13 02:39:52 +0700

    add feature delete

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -34,10 +34,31 @@ let displayGetCRUD = async (req, res) => {
     dataTable: data,
   });
 };
+let getEditCRUD = async (req, res) => {
+  let userId = req.query.id;
+  console.log(userId);
+  if (userId) {
+    let userData = await CRUDService.getUserInfoById(userId);
+    return res.render("editCRUD.ejs", {
+      user: userData,
+    });
+  } else {
+    return res.send("Errol!");
+  }
+};
+let putCRUD = async (req, res) => {
+  let data = req.body;
+  let allUser = await CRUDService.updateUserData(data);
+  return res.render("displayCRUD.ejs", {
+    dataTable: allUser,
+  });
+};
 module.exports = {
   getHomePage: getHomePage,
   getTestapi: getTestapi,
   getCRUD: getCRUD,
   postCRUD: postCRUD,
   displayGetCRUD: displayGetCRUD,
+  getEditCRUD: getEditCRUD,
+  putCRUD: putCRUD,
 };
--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -48,7 +48,49 @@ let getAllUser = () => {
     }
   });
 };
+let getUserInfoById = (userId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: userId },
+        raw: true,
+      });
+      if (user) {
+        resolve(user);
+      } else {
+        resolve({});
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+let updateUserData = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: data.id },
+      });
+      if (user) {
+        user.firstName = data.firstName;
+        user.lastName = data.lastName;
+        user.address = data.address;
+        user.phonenumber = data.phonenumber;
+        await user.save();
+        let allUser = db.User.findAll({});
+        resolve(allUser);
+      } else {
+        resolve();
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+  console.log(data);
+};
 module.exports = {
   createNewUser: createNewUser,
   getAllUser: getAllUser,
+  getUserInfoById: getUserInfoById,
+  updateUserData: updateUserData,
 };
